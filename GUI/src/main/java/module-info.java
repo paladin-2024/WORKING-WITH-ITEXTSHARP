3f16f6cc29608fc85604a6b257d7fdfed456bc6b
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-03-31 01:59:42 +0300

    add employee and department management panels with CRUD functionality and PDF export feature

--- GUI/src/main/java/module-info.java
+++ GUI/src/main/java/module-info.java
@@ -2,10 +2,11 @@ module GUI {
     exports gui;
     exports util;
     requires Utils;
-    requires java.sql;
     requires itextpdf;
     requires kernel;
     requires layout;
     requires jdk.hotspot.agent;
     requires java.desktop;
+    requires Core;
+    requires java.sql;
 }
